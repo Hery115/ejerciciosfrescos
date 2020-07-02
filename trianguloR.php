add54036f22059b06c81022b5b99b00697122225
Hery115 <65785203+Hery115@users.noreply.github.com>
2020-07-01 20:04:32 -0500

    Add files via upload

---
+++ trianguloR.php
@@ -0,0 +1,17 @@
+<?php
+  $a = $_POST['letraA'];
+  $b = $_POST['letraB'];
+  $c = $_POST['letraC'];
+
+
+ $aArriba= $a-$c;
+
+ $r1 = $b * $c;
+
+ $r2 = ($B * $aDeArriba)/2;
+
+ $rf = $r1 + $r2;
+
+ echo "El area de la figura es: $rt";
+
+?>
